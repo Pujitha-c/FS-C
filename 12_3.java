564300145851cff288242eb6ca99546c425f25a8
Pujitha-c <69040908+Pujitha-c@users.noreply.github.com>
2021-02-04 18:37:21 +0530

    InsertInArray

---
+++ 12_3.java
@@ -0,0 +1,48 @@
+// Q3) Java Program to Insert an Element in a Specified Position in a Given Array
+
+
+/* Sample Input & Output:
+Enter no. of elements you want in array:6
+Enter all the elements:
+2
+4
+6
+9
+4
+5
+Enter the position where you want to insert element:3
+Enter the element you want to insert:7
+After inserting:2,4,7,6,9,4,5
+*/
+import java.util.*;
+public class Insert_Array
+{
+    public static void main(String[] arhs)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a[]=new int[30];
+        System.out.print("Enter no. of elements you want in array:");
+        int n=sc.nextInt();
+        System.out.println("Enter all the elements:");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.print("Enter the position where you want to insert element:");
+        int pos=sc.nextInt();
+        System.out.print("Enter the element you want to insert:");
+        int ele=sc.nextInt();
+        for(int i=n;i>pos;i--)
+        {
+            a[i]=a[i-1];
+        }
+        a[pos]=ele;
+        System.out.print("After inserting:");
+        for(int i=0;i<n+1;i++)
+        {
+            System.out.print(a[i]);
+            if(i<=n-1)
+            System.out.print(",");
+        }
+    }
+}
